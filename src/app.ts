43bfd61405a9f61606172e1dbb8859bb2127962c
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2023-11-15 21:54:45 +0700

    Add static folder

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import createError from "http-errors";
 import express, { ErrorRequestHandler } from "express";
-import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import "dotenv/config";
@@ -13,7 +12,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.use("/", indexRouter);
 
